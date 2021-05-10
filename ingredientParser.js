ae4a6142460d4861c2180559a5d106b17ccc5c0d
Harry Cameron <hcphoon01@gmail.com>
2021-05-10 18:02:03 +0100

    feat: slightly changed ingredient parser

--- ingredientParser.js
+++ ingredientParser.js
@@ -1,53 +1,76 @@
 const fs = require("fs");
 
-const recipes = JSON.parse(fs.readFileSync("output.json"));
+parseIngredients("test.json", 100);
 
-const map = new Map();
-const output = [];
+function parseIngredients(input, trim, modifications = true) {
+  const recipes = JSON.parse(fs.readFileSync(input));
 
-for (let i = 0; i < recipes.length; i++) {
-  const recipe = recipes[i];
-  try {
-    for (let i = 0; i < recipe.ingredients.length; i++) {
-      let ingredient = recipe.ingredients[i];
-      ingredient = ingredient.split(",")[0];
-      ingredient = ingredient.replace(/[a-z]*\d+[a-z]*/gi, "");
-      ingredient = ingredient.replace("½", "");
-      ingredient = ingredient.replace("¼", "");
-      ingredient = ingredient.replace(".", "");
-      ingredient = ingredient.replace("- ", "");
-      ingredient = ingredient.replace("x ", "");
-      ingredient = ingredient.replace("  ", " ");
-      ingredient = ingredient.replace("about ", "");
-      ingredient = ingredient.replace("litre ", "");
-      ingredient = ingredient.replace("can ", "");
-      ingredient = ingredient.replace("small ", "");
-      ingredient = ingredient.replace("large ", "");
-      ingredient = ingredient.replace("/ ", "");
-      ingredient = ingredient.split(" or ")[0];
-      ingredient = ingredient.split("(")[0];
+  const map = new Map();
+  const output = [];
 
-      if (ingredient.includes(" of ")) {
-        ingredient = ingredient.split(" of ")[1];
-      } else if (ingredient.includes("tbsp")) {
-        ingredient = ingredient.split("tbsp")[1];
-      } else if (ingredient.includes("tsp")) {
-        ingredient = ingredient.split("tsp")[1];
+  for (let i = 0; i < recipes.length; i++) {
+    const recipe = recipes[i];
+    try {
+      for (let i = 0; i < recipe.ingredients.length; i++) {
+        let ingredient = recipe.ingredients[i];
+        if (modifications) {
+          ingredient = ingredient.split(",")[0];
+          ingredient = ingredient.replace(/[a-z]*\d+[a-z]*/gi, "");
+          ingredient = ingredient.replace("½", "");
+          ingredient = ingredient.replace("¼", "");
+          ingredient = ingredient.replace(".", "");
+          ingredient = ingredient.replace("- ", "");
+          ingredient = ingredient.replace("x ", "");
+          ingredient = ingredient.replace("  ", " ");
+          ingredient = ingredient.replace("about ", "");
+          ingredient = ingredient.replace("litre ", "");
+          ingredient = ingredient.replace("can ", "");
+          ingredient = ingredient.replace("small ", "");
+          ingredient = ingredient.replace("large ", "");
+          ingredient = ingredient.replace("/ ", "");
+          ingredient = ingredient.split(" or ")[0];
+          ingredient = ingredient.split("(")[0];
+
+          if (ingredient.includes(" of ")) {
+            ingredient = ingredient.split(" of ")[1];
+          } else if (ingredient.includes("tbsp")) {
+            ingredient = ingredient.split("tbsp")[1];
+          } else if (ingredient.includes("tsp")) {
+            ingredient = ingredient.split("tsp")[1];
+          }
+        }
+        ingredient = ingredient.trim();
+
+        ingredient = titleCase(ingredient);
+
+        if (map.has(ingredient)) {
+          map.set(ingredient, map.get(ingredient) + 1);
+        } else {
+          map.set(ingredient, 1);
+        }
       }
-      ingredient = ingredient.trim();
-
-      ingredient = titleCase(ingredient);
-
-      if (map.has(ingredient)) {
-        map.set(ingredient, map.get(ingredient) + 1);
-      } else {
-        map.set(ingredient, 1);
-      }
+    } catch (e) {
+      console.log(e);
+      console.log(recipe.title);
     }
-  } catch (e) {
-    console.log(e);
-    console.log(recipe.title);
   }
+
+  const mapAscending = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
+  const mapDescending = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
+
+  for (const elem of map.entries()) {
+    if (elem[1] >= trim) {
+      output.push({
+        name: elem[0],
+        category: 0,
+      });
+    }
+  }
+
+  fs.writeFileSync("common-ingredients.json", JSON.stringify(output));
+
+  //console.log(mapDescending);
+  //console.log(mapAscending);
 }
 
 function titleCase(str) {
@@ -58,20 +81,3 @@ function titleCase(str) {
   }
   return splitStr.join(" ");
 }
-
-const mapAscending = new Map([...map.entries()].sort((a, b) => a[1] - b[1]));
-const mapDescending = new Map([...map.entries()].sort((a, b) => b[1] - a[1]));
-
-for (const elem of map.entries()) {
-  if (elem[1] >= 30) {
-    output.push({
-      'name': elem[0],
-      'category': 0
-    });
-  }
-}
-
-fs.writeFileSync('common-ingredients.json', JSON.stringify(output));
-
-//console.log(mapDescending);
-//console.log(mapAscending);
