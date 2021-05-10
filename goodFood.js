968b89b9e3d50b930bc4870be984bc3c575e6183
Harry Cameron <hcphoon01@gmail.com>
2021-05-10 14:48:29 +0100

    feat: improved goodFood parser

--- goodFood.js
+++ goodFood.js
@@ -4,13 +4,14 @@ const fs = require("fs");
 
 const baseUrl = "https://www.bbcgoodfood.com";
 const searchUrl = "https://www.bbcgoodfood.com/search/recipes/page/";
-const pages = 2; // Hard coded amount of pages of recipes, max = 417
+const startPage = 1;
+const endPage = 100; // Hard coded amount of pages of recipes, max = 417
 
 const recipeUrls = [];
 const recipes = [];
 
 async function getUrls() {
-  for (let i = 1; i <= pages; i++) {
+  for (let i = startPage; i <= endPage; i++) {
     await axios.get(searchUrl + i).then((res) => {
       const $ = cheerio.load(res.data);
       $("a.standard-card-new__article-title").each((index, value) => {
@@ -23,37 +24,54 @@ async function getUrls() {
 async function getRecipes() {
   for (let i = 0; i < recipeUrls.length; i++) {
     const url = recipeUrls[i];
-    await axios.get(url).then((res) => {
-      const recipe = {
-        title: "",
-        image: "",
-        sourceName: "BBC Good Food",
-        sourceUrl: url,
-        ingredients: [],
-        method: [],
-      };
-      const $ = cheerio.load(res.data);
-      recipe.title = $(".post-header__title").text();
-      let img = $(".image__img").attr("src");
-      recipe.image = img.split("?")[0];
-      $(".recipe__ingredients")
-        .find("li")
-        .each((index, value) => {
-          recipe.ingredients.push($(value).text());
-        });
-      $(".recipe__method-steps")
-        .find("p")
-        .each((index, value) => {
-          recipe.method.push($(value).text());
-        });
-      recipes.push(recipe);
+    await axios
+      .get(url)
+      .then((res) => {
+        const recipe = {
+          title: "",
+          image: "",
+          sourceName: "BBC Good Food",
+          sourceUrl: url,
+          ingredients: [],
+          method: [],
+        };
+        const $ = cheerio.load(res.data);
+        recipe.title = $(".post-header__title").text();
+        let img = $(".image__img").attr("src");
+        recipe.image = img.split("?")[0];
+        $(".recipe__ingredients")
+          .find("li")
+          .each((index, value) => {
+            recipe.ingredients.push($(value).text());
+          });
+        $(".recipe__method-steps")
+          .find("p")
+          .each((index, value) => {
+            recipe.method.push($(value).text());
+          });
+        recipes.push(recipe);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+function writeToFile() {
+  if (fs.existsSync("output.json")) {
+    fs.readFile("output.json", function (err, data) {
+      var json = JSON.parse(data);
+      json.push(recipes);
+      fs.writeFileSync("output.json", JSON.stringify(json));
     });
+  } else {
+    fs.writeFileSync("output.json", JSON.stringify(recipes));
   }
 }
 
 getUrls().then(() => {
+  console.log(recipeUrls.length);
   getRecipes().then(() => {
-    let json = JSON.stringify(recipes);
-    fs.writeFileSync("output.json", json);
+    writeToFile();
   });
 });
